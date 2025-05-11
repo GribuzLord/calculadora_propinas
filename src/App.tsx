d88ec56085b0a1e1117637a22a38047f30a2aa35
GribuzLord <gribuzcode@gmail.com>
2025-05-10 21:12:42 -0600

    Se conecto el componente menuItem con las props de la base de datos simulada y se muestran en pantalla con un poco de estilos y formatos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
 
@@ -13,10 +14,17 @@ function App() {
 
     <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
 
-      <div>
-        <h2>Menú</h2>
-
-
+      <div className="p-5">
+        <h2 className="text-4xl font-black">Menú</h2>
+        <div className="space-y-3 mt-10">
+          {menuItems.map(item=>(
+            <MenuItem
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </div>
+        
       </div>
       
       <div>
