f4cd52a9ac37731f6c5abdebe7c09bf6656959f8
GribuzLord <gribuzcode@gmail.com>
2025-05-10 22:29:16 -0600

    Se modificó la funcion del custom hook para que este pudisese recibir un parametro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 
 function App() {
 
-  
+  const {addItem}=useOrder()
 
   return (
     <>
@@ -21,6 +22,7 @@ function App() {
             <MenuItem
               key={item.id}
               item={item}
+              addItem={addItem}
               
             />
           ))}
