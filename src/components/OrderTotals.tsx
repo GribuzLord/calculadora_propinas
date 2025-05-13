b732e596a7307385e5729499bed0c5cb56aa040d
GribuzLord <gribuzcode@gmail.com>
2025-05-13 17:46:24 -0600

    Se creó la ultima variable para hacer todos los calculos de los totales, en este caso es el precio final

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -12,13 +12,14 @@ export default function OrderTotals({order,tip}:OrderTottalsProps) {
 
     const subtotalAmount=useMemo(() => order.reduce((total,item)=>total+(item.quantity*item.price),0) , [order])
     const tipAmount=useMemo(()=>subtotalAmount*tip,[tip,order])
+    const totalAmount=useMemo(()=>subtotalAmount+tipAmount,[tip,order])
 
     return (
     <>
         <div className="space-y-3">
             <h2 className="font-black text-2xl">Totales y Propinas:</h2>
             <p>Subtotal a pagar:
-                <span className="font-bold ">{formatCurrency(subtotalAmount)}</span>
+                <span className="font-bold "> {formatCurrency(subtotalAmount)}</span>
             </p>
 
             <p>Propina:
@@ -26,7 +27,7 @@ export default function OrderTotals({order,tip}:OrderTottalsProps) {
             </p>
 
             <p>Total a pagar:
-                <span className="font-bold "> $0</span>
+                <span className="font-bold "> {formatCurrency(totalAmount)}</span>
             </p>
         </div>
     
