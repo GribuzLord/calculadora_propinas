1be0611b3c1674555373001e6be3e4cbc971d95e
GribuzLord <gribuzcode@gmail.com>
2025-05-13 16:49:56 -0600

    Se conecto el componnete para mostrar los totales al componente para mostrar la orden y con esto se puede hacer el calculo del subtotal a pagar

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,10 +1,25 @@
-export default function OrderTotals() {
-  return (
+import { useMemo } from "react"
+import type { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+
+type OrderTottalsProps={
+    order:OrderItem[]
+}
+
+export default function OrderTotals({order}:OrderTottalsProps) {
+  
+  
+
+
+    const subtotalAmount=useMemo(() => order.reduce((total,item)=>total+(item.quantity*item.price),0) , [order])
+
+    return (
     <>
         <div className="space-y-3">
             <h2 className="font-black text-2xl">Totales y Propinas:</h2>
             <p>Subtotal a pagar:
-                <span className="font-bold "> $0</span>
+                <span className="font-bold ">{formatCurrency(subtotalAmount)}</span>
             </p>
 
             <p>Propina:
